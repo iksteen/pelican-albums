f09885b2c268121a26e81778233eeafcd312585c
Ingmar Steen <iksteen@gmail.com>
2015-04-13 11:55:18 +0200

    Automatically exclude album path.
    Adds the album path to PAGE_EXCLUDES and ARTICLE_EXCLUDES.

--- pelican_albums/__init__.py
+++ pelican_albums/__init__.py
@@ -1,3 +1,4 @@
+import os
 from pelican import signals
 from . import thumbnails, generator, content
 
@@ -8,6 +9,13 @@ def initialized(pelican):
     pelican.settings.setdefault('THUMBNAIL_DEFAULT_SIZE', '192x192')
     pelican.settings.setdefault('ALBUM_SAVE_AS', 'albums/{slug}.html')
     pelican.settings.setdefault('ALBUM_URL', 'albums/{slug}.html')
+
+    image_path = os.path.join(
+        pelican.settings['PATH'],
+        pelican.settings['ALBUM_PATH']
+    )
+    pelican.settings.setdefault('ARTICLE_EXCLUDES', []).append(image_path)
+    pelican.settings.setdefault('PAGE_EXCLUDES', []).append(image_path)
 
 
 def register():
